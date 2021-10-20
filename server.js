53a72f8eeb03b3151b3d8dd3e15aef9cc5d973ae
juanhdev <juanhlealbarros@gmail.com>
2021-10-19 22:06:17 -0300

    Link css and font-size

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
 app.set('view engine', 'ejs');
+app.use(express.static('public'));
 
 app.get('/', (req, res) => {
   const welcomeMessage = `#Welcome to Wastebin!
