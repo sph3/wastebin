acc9945887e7f241efae4315171176e5af87b984
juanhdev <juanhlealbarros@gmail.com>
2021-10-19 21:34:39 -0300

    Basic server and EJS setup

---
+++ server.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const app = express();
+app.set('view engine', 'ejs');
+
+app.get('/', (req, res) => {
+  const welcomeMessage = `#Welcome to Wastebin!
+
+Paste your code here and use the commands
+in the top right corner to create
+a new file and share with others.`;
+
+  res.render('code', { code: welcomeMessage });
+});
+
+app.listen(8000);
