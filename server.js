01e3380a0c84f5acf3ab16a33296f040c68a55a1
juanhdev <juanhlealbarros@gmail.com>
2021-10-19 22:08:50 -0300

    Huh

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ app.use(express.static('public'));
 
 app.get('/', (req, res) => {
   const welcomeMessage = `#Welcome to Wastebin!
-
+  
 Paste your code here and use the commands
 in the top right corner to create
 a new file and share with others.`;
